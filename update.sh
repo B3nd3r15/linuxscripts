666cdbe3002abcc8d6c1ee6a4a98f81d757a8675
B3nd3r15 <beaton0506@gmail.com>
2018-09-26 13:11:39 -0500

    updated template

--- update.sh
+++ update.sh
@@ -3,6 +3,8 @@
 ###################################################################
 # Script Name: update.sh
 #
+# Date Created: 2018-09-01
+#
 # Description: script to detect which package manger you are using
 # install updates, cleans up old pacakges, installs ntp if needed,
 # configure ntp with google time servers, and check for new LTS 
