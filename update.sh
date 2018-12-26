0bc4b19662168ee7536c68800c909888affa1c76
B3nd3r15 <beaton0506@gmail.com>
2018-12-26 13:48:29 -0600

    changed output formatting

--- update.sh
+++ update.sh
@@ -80,8 +80,6 @@ LOG_LOCATION=/var/log
 exec > >(tee -ai $LOG_LOCATION/"${scriptname}".log )
 exec 2>&1
 echo ""
-echo "Log Location: [ $LOG_LOCATION ]"
-echo ""
 
 #--------------------------------------------------
 #	Determine Installed packaging system
@@ -93,84 +91,58 @@ if [[ -n $YUM_CMD ]]; then
 		#	Update
 		#---------------------------------
 		echo "" 
-		echo "################################################################################" 
 		echo "# Upgrading $osver on $(timestamp) #" 
-		echo "################################################################################" 
-		echo "" 
 		
 		#---------------------------------
 		#	Update Yum
 		#---------------------------------
 		echo ""
 		echo -e "\xE2\x9C\x94" Updating Yum
-		echo ""
-		yes | sudo yum update -y | sed "s/$/ [$(date +"%Y-%m-%d %T")]/"
-		echo ""
+		yes | sudo yum update -y >> $LOG_LOCATION/"${scriptname}".log
 		
 		#---------------------------------
 		#	Install Updates
 		#---------------------------------
-		echo ""
 		echo -e "\xE2\x9C\x94" Installing Updates
-		echo ""
-		yes | sudo yum upgrade | sed "s/$/ [$(date +"%Y-%m-%d %T")]/"
-		echo ""
+		yes | sudo yum upgrade >> $LOG_LOCATION/"${scriptname}".log
 
 		#---------------------------------
 		#	Clean up unused pacakages
 		#---------------------------------
-		echo ""
 		echo -e "\xE2\x9C\x94" Cleaning Up Yum Packages
-		echo ""
-		yes | sudo yum clean packages | sed "s/$/ [$(date +"%Y-%m-%d %T")]/"
-		echo ""
+		yes | sudo yum clean packages >> $LOG_LOCATION/"${scriptname}".log
 
 		#---------------------------------
 		#	Clean up Yum Metadata
 		#---------------------------------
-		echo ""
 		echo -e "\xE2\x9C\x94" Cleaning Up Yum Metadata
-		echo ""
-		yes | sudo yum clean metadata | sed "s/$/ [$(date +"%Y-%m-%d %T")]/"
-		echo ""
+		yes | sudo yum clean metadata >> $LOG_LOCATION/"${scriptname}".log
 
 		#---------------------------------
 		#	Clean Yum DB Cache
 		#---------------------------------
-		echo ""
 		echo -e "\xE2\x9C\x94" Cleaning Up Yum DBCache
-		echo ""
-		yes | sudo yum clean dbcache | sed "s/$/ [$(date +"%Y-%m-%d %T")]/"
-		echo ""
+		yes | sudo yum clean dbcache >> $LOG_LOCATION/"${scriptname}".log
 
 		#---------------------------------
 		#	Clean anything leftover
 		#---------------------------------
-		echo ""
 		echo -e "\xE2\x9C\x94" Cleaning up Yum Everything
-		echo ""
-		yes | sudo yum clean all | sed "s/$/ [$(date +"%Y-%m-%d %T")]/"
-		echo ""
+		yes | sudo yum clean all >> $LOG_LOCATION/"${scriptname}".log
 
 		#---------------------------------
 		#	Remove /var/cache/yum file
 		#---------------------------------
-		echo ""
 		echo -e "\xE2\x9C\x94" Removing /var/cache/yum
-		yes | sudo rm -rf /var/cache/yum | sed "s/$/ [$(date +"%Y-%m-%d %T")]/"
-		echo ""
+		yes | sudo rm -rf /var/cache/yum >> $LOG_LOCATION/"${scriptname}".log
 
 		echo "" 
-		echo "################################################################################" 
 		echo "# End of Upgrade on $(timestamp) #" 
-		echo "################################################################################" 
 		echo "" 
 
-		echo "" 
-		echo "################################################################################" 
-		echo "# Start disable of Chronyd Service on $(timestamp) #"
-		echo "################################################################################" 
-		echo "" 
+		#echo "" 
+		#echo "# Start disable of Chronyd Service #"
+		#echo "" 
 
 		if P=$(pgrep $SERVICE); then
    			echo "$SERVICE is running, PID is $P, Disabling chronyd service."
@@ -179,52 +151,36 @@ if [[ -n $YUM_CMD ]]; then
    			#Disable chronyd so it cannot start if server reboots.
    			systemctl disable chronyd
 		else
-   			echo "$SERVICE is not running or has been disabled."
+   			echo -e "\xE2\x9C\x94" "$SERVICE is not running or has been disabled."
    		fi
-
-		echo "" 
-		echo "################################################################################" 
-		echo "# End of disabling Chronyd Service on $(timestamp) #"
-		echo "################################################################################" 
-		echo "" 
-
-		echo ""
- 	    echo "################################################################################"
- 	    echo "# Configuring NTP on $(timestamp) #"
- 	    echo "################################################################################"
- 	    echo ""
 
     	#-------------------------------------------------------------------------------------
 		# Checks to see if NTP is installed. If it is, continues to check if the config file
     	# is modified if not it will install it and update the config file
 		#-------------------------------------------------------------------------------------
-    	if yum list installed | grep ntp.x86_64 > /dev/null 2>&1 | sed "s/$/ [$(date +"%Y-%m-%d %T")]/"; then
+    	if yum list installed | grep ntp.x86_64 > /dev/null 2>&1; then
     		echo ""
-    		echo -e "\xE2\x9C\x94" NTP Successfully Installed | sed "s/$/ [$(date +"%Y-%m-%d %T")]/"
+    		echo -e "\xE2\x9C\x94" NTP Successfully Installed
 		else
-			echo ""
-    		echo -e "\xE2\x9C\x94" Installing NTP
-    		yes | sudo yum install ntp ntpd | sed "s/$/ [$(date +"%Y-%m-%d %T")]/"
+			echo -e "\xE2\x9C\x94" Installing NTP
+    		yes | sudo yum install ntp ntpd >> $LOG_LOCATION/"${scriptname}".log
 		fi  
 
 		#-------------------------------------------------
 		# Checks to see if the config files need updated
 		#-------------------------------------------------
 		if grep google.com /etc/ntp.conf > /dev/null 2>&1; then
- 			echo ""
- 			echo -e "\xE2\x9C\x94" NTP conf file already updated. | sed "s/$/ [$(date +"%Y-%m-%d %T")]/"
+ 			echo -e "\xE2\x9C\x94" NTP conf file already updated.
 		else
-			echo ""
-			echo -e "\xE2\x9C\x94" Updating NTP conf file | sed "s/$/ [$(date +"%Y-%m-%d %T")]/"
+			echo -e "\xE2\x9C\x94" Updating NTP conf file
 	
 
-		#-----------------------------------------------\------------------------------------
+		#------------------------------------------------------------------------------------
 		# The config files for ntp lies in /etc/ntp.conf
 		# We are changing the Servers time to google's public NTP servers
 		# Look here for more info : https://developers.google.com/time/guides#linux_ntpd
 		#-----------------------------------------------------------------------------------
-			echo "" 
-			echo -e "\xE2\x9C\x94" Modifying NTP config file | sed "s/$/ [$(date +"%Y-%m-%d %T")]/"
+			echo -e "\xE2\x9C\x94" Modifying NTP config file
 			
 			#-------------------------------------------------
 			# Comment out the default pool servers.
@@ -243,8 +199,7 @@ if [[ -n $YUM_CMD ]]; then
 			#-------------------------------------------------
 			# Restart, enable, and show the status of the service
 			#-------------------------------------------------
-			echo "" 
-			echo -e "\xE2\x9C\x94" Restarting NTP Service | sed "s/$/ [$(date +"%Y-%m-%d %T")]/"
+			echo -e "\xE2\x9C\x94" Restarting NTP Service
 			sudo systemctl stop ntpd
 			sleep 2
 			sudo systemctl start ntpd
@@ -258,14 +213,12 @@ if [[ -n $YUM_CMD ]]; then
 		# Give ntp service time to start up and talk to time*.google.com
 		#-------------------------------------------------
 		sleep 5
-		echo ""
-		echo -e "\xE2\x9C\x94" Waiting for NTP service to start | sed "s/$/ [$(date +"%Y-%m-%d %T")]/"
+		echo -e "\xE2\x9C\x94" Waiting for NTP service to start
 
 		#-------------------------------------------------
 		# Show NTP servers
 		#-------------------------------------------------
-		echo "" 
-		echo -e "\xE2\x9C\x94" Showing current NTP Servers | sed "s/$/ [$(date +"%Y-%m-%d %T")]/"
+		echo -e "\xE2\x9C\x94" Showing current NTP Servers
 		echo ""
 		ntpq -p
 		echo ""
@@ -273,10 +226,8 @@ if [[ -n $YUM_CMD ]]; then
 		echo ""
 
 		echo ""
- 	    echo "################################################################################"
- 	    echo "# Completed NTP Configuration on $(timestamp) #"
- 	    echo "################################################################################"
- 	    echo ""
+		echo "To view the log file: [ less $LOG_LOCATION/"${scriptname}".log ]"
+		echo ""
 
 elif [[ -n $APT_GET_CMD ]]; then
 
@@ -284,57 +235,38 @@ elif [[ -n $APT_GET_CMD ]]; then
  	    # Update all the things!
 		#---------------------------------
  		echo ""
-  		echo "################################################################################"
   		echo "# Upgrading $osver on $(timestamp) #"
-  		echo "################################################################################"
   		echo ""
 
   		#---------------------------------
 	    # Update all the repos.
 		#---------------------------------
-   		echo ""
-   		echo -e "\xE2\x9C\x94" Updating Repos 
-   		echo ""
-   		yes | sudo apt-get update | sed "s/$/ [$(date +"%Y-%m-%d %T")]/"
-   		echo ""
+   		echo -e "\xE2\x9C\x94" Updating Repos
+   		yes | sudo apt-get update >> $LOG_LOCATION/"${scriptname}".log
 
    		#---------------------------------
  	    # Upgrade all the things.
 		#---------------------------------
-		echo ""
- 		echo -e "\xE2\x9C\x94" Upgrading System 
- 		echo ""
- 		yes | sudo apt-get dist-upgrade | sed "s/$/ [$(date +"%Y-%m-%d %T")]/"
-   		echo ""
+ 		echo -e "\xE2\x9C\x94" Upgrading System
+ 		yes | sudo apt-get dist-upgrade >> $LOG_LOCATION/"${scriptname}".log
 
    		#---------------------------------
 	    # Remove old software.
 		#---------------------------------
-    	echo ""
  		echo -e "\xE2\x9C\x94" Removing Unused Software
- 		echo ""
- 		yes|sudo apt-get autoremove | sed "s/$/ [$(date +"%Y-%m-%d %T")]/"
-    	echo ""
+ 		yes|sudo apt-get autoremove >> $LOG_LOCATION/"${scriptname}".log
 
     	#---------------------------------
 	    # Purge config files
 		#---------------------------------
-    	echo ""
- 		echo -e "\xE2\x9C\x94" Purging Leftover Config Files 
- 		echo ""
- 		apt-get purge -y "$(dpkg -l | awk '/^rc/ { print $2 }')" | sed "s/$/ [$(date +"%Y-%m-%d %T")]/"
-    	echo ""
+ 		echo -e "\xE2\x9C\x94" Purging Leftover Config Files
+ 		apt-get purge -y "$(dpkg -l | awk '/^rc/ { print $2 }')" >>$LOG_LOCATION/"${scriptname}".log
 
     	echo ""
-    	echo "################################################################################"
     	echo "# End of Upgrade on $(timestamp) #"
-    	echo "################################################################################"
-    	echo "" 
 
     	echo ""
- 	    echo "################################################################################"
  	    echo "# Checking for new LTS release on $(timestamp) #"
- 	    echo "################################################################################"
  	    echo ""
 
     	#---------------------------------
@@ -355,15 +287,11 @@ elif [[ -n $APT_GET_CMD ]]; then
 		fi
 
 		echo ""
- 	    echo "################################################################################"
  	    echo "# Completed Checking for LTS release on $(timestamp) #"
- 	    echo "################################################################################"
  	    echo ""
 
  	    echo ""
- 	    echo "################################################################################"
  	    echo "# Configuring NTP on $(timestamp) #"
- 	    echo "################################################################################"
  	    echo ""
 
  	    #---------------------------------
@@ -371,19 +299,16 @@ elif [[ -n $APT_GET_CMD ]]; then
     	# if not it will install it. 
 		#---------------------------------
     	if apt-get -qq install ntp ntpstat; then 
-    		echo ""
-    		echo -e "\xE2\x9C\x94" NTP Successfully Installed | sed "s/$/ [$(date +"%Y-%m-%d %T")]/"
+    		echo -e "\xE2\x9C\x94" NTP Successfully Installed
 		else
-			echo ""
-    		yes | sudo apt-get install ntp ntpstat | sed "s/$/ [$(date +"%Y-%m-%d %T")]/"
+    		yes | sudo apt-get install ntp ntpstat >> $LOG_LOCATION/"${scriptname}".log
 		fi  
 
 		#---------------------------------
 		# Checks to see if the config files need updated
 		#---------------------------------
 		if grep google.com /etc/ntp.conf > /dev/null 2>&1; then
- 			echo ""
- 			echo -e "\xE2\x9C\x94" NTP config file already updated. | sed "s/$/ [$(date +"%Y-%m-%d %T")]/"
+ 			echo -e "\xE2\x9C\x94" NTP config file already updated.
 		else
 
 		#---------------------------------
@@ -392,7 +317,7 @@ elif [[ -n $APT_GET_CMD ]]; then
 		# Look here for more info : https://developers.google.com/time/guides#linux_ntpd
 		#---------------------------------
 			echo "" 
-			echo -e "\xE2\x9C\x94" Modifying NTP config file | sed "s/$/ [$(date +"%Y-%m-%d %T")]/"
+			echo -e "\xE2\x9C\x94" Modifying NTP config file
 	
 		#---------------------------------
 		# Comment out the default pool servers.
@@ -412,7 +337,7 @@ elif [[ -n $APT_GET_CMD ]]; then
 		# Restart the NTP service.
 		#---------------------------------
 			echo "" 
-			echo -e "\xE2\x9C\x94" Restarting NTP Service | sed "s/$/ [$(date +"%Y-%m-%d %T")]/"
+			echo -e "\xE2\x9C\x94" Restarting NTP Service
 			echo ""
 			sudo systemctl stop ntp
 			sleep 2
@@ -426,15 +351,14 @@ elif [[ -n $APT_GET_CMD ]]; then
  		# Sleep 5 seconds to give the service time to start and talk to the servers
 		#---------------------------------
 			echo ""
-			echo -e "\xE2\x9C\x94" Waiting for NTP service to start | sed "s/$/ [$(date +"%Y-%m-%d %T")]/"
+			echo -e "\xE2\x9C\x94" Waiting for NTP service to start
 			sleep 5
 		fi
 		
 		#---------------------------------
 		# Show NTP servers
 		#---------------------------------
-		echo "" 
-		echo -e "\xE2\x9C\x94" Showing current NTP Servers | sed "s/$/ [$(date +"%Y-%m-%d %T")]/"
+		echo -e "\xE2\x9C\x94" Showing current NTP Servers
 		echo ""
 		ntpq -p 
 		echo ""
@@ -442,15 +366,17 @@ elif [[ -n $APT_GET_CMD ]]; then
 		echo ""
 
 		echo ""
- 	    echo "################################################################################"
  	    echo "# Completed NTP Configuration on $(timestamp) #"
- 	    echo "################################################################################"
  	    echo ""
+
+ 	    echo ""
+		echo "To view the log file: [ less $LOG_LOCATION/"${scriptname}".log ]"
+		echo ""
 
 #---------------------------------
 # If neither Yum or Apt are installed, exit and have user manually install updates on their system.
 #---------------------------------
 else
 	echo "Cannot determine installed packaging system, Please manually update." | sed "s/$/ [$(date +"%Y-%m-%d %T")]/"
- 	exit 1; 
+ 	exit 1;
 fi
