3b673c8987aeb8e5c212286de38743122cb587cd
B3nd3r15 <beaton0506@gmail.com>
2019-02-25 15:30:17 -0600

    added version info in change log

--- update.sh
+++ update.sh
@@ -21,7 +21,8 @@
 #
 #    Version      AUTHOR      DATE          COMMENTS
 #                 ------      ----          --------
-#  VER 0.2.0      B3nder      2019/02/18    sent echo commands to /dev/null so it doesn't fill up the screen.
+#  VER 0.3.0      B3nd3r      2019/02/25    Created TCPBBR function to configure tcp to use bbr congestion control.
+#  VER 0.2.0      B3nd3r      2019/02/18    sent echo commands to /dev/null so it doesn't fill up the screen.
 #  VER 0.1.0      B3nd3r      2019/01/23    Initial creation and release.
 #
 #################################################################################################################
